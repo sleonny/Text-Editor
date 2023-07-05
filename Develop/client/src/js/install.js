72e3a7dd3834751d2e895437f79af284d97708ff
Sean L <leonardmsean@gmail.com>
2023-07-05 16:29:17 -0400

    changed butInstall to buttonIntall to match html id

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,23 +1,21 @@
-const butInstall = document.getElementById("buttonInstall");
+const buttonInstall = document.getElementById("buttonInstall");
 
 let deferredPrompt;
 
 window.addEventListener("beforeinstallprompt", (event) => {
-    event.preventDefault();
-    deferredPrompt = event;
-    butInstall.style.display = 'block';
+  event.preventDefault();
+  deferredPrompt = event;
+  buttonInstall.style.display = "block";
 });
-
 
 butInstall.addEventListener("click", async () => {
-    butInstall.style.display = 'none';
-    deferredPrompt.prompt();
-    const { outcome } = await deferredPrompt.userChoice;
-    console.log('User response to the install prompt: ${outcome}');
-    deferredPrompt = null;
+  buttonInstall.style.display = "none";
+  deferredPrompt.prompt();
+  const { outcome } = await deferredPrompt.userChoice;
+  console.log("User response to the install prompt: ${outcome}");
+  deferredPrompt = null;
 });
 
-
 window.addEventListener("appinstalled", (event) => {
-    console.log('The app has been installed sucessfully');
+  console.log("The app has been installed sucessfully");
 });

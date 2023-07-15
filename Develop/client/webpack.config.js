94fbecdd4b0083bf0703b3a14d27ee0ff325c962
Sean L <leonardmsean@gmail.com>
2023-07-15 13:38:38 -0400

    fixed some bugs in webpack.config.js

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: "development",
@@ -19,31 +16,36 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: "./src/index.html",
-        filename: "index.html",
+        template: "./index.html",
+        title: "Just Another Text Editor",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
       new WebpackPwaManifest({
-        name: "My Progressive Web App",
-        short_name: "MyPWA",
-        description: "My Progressive Web App",
-        background_color: "#ffffff",
-        crossorgin: "use-credentials",
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "Just Another Text Editor",
+        background_color: "#710C04",
+        start_url: "/",
+        publicPath: "/",
+
         icons: [
           {
-            src: path.resolve("src/assets/icon.png"),
+            src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join("icons"),
           },
         ],
       }),
-      new InjectManifest({
-        swSrc: "../client/src-sw.js",
-        swDest: "sw.js",
-      }),
     ],
 
     module: {
       rules: [
+        { test: /\.css$/i, use: ["style-loader", "css-loader"] },
         {
           test: /\.js$/,
           exclude: /node_modules/,
@@ -51,10 +53,13 @@ module.exports = () => {
             loader: "babel-loader",
             options: {
               presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
         },
-        { test: /\.css$/i, use: ["style-loader", "css-loader"] },
       ],
     },
   };
